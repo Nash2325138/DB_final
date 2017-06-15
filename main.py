602c03c9e33f8065729117c9c2f4c076f3e661d4
劉哲宇 <j2325138@gmail.com>
2017-06-15 19:33:36 +0800

    Everyone is writing compiler

--- main.py
+++ main.py
@@ -4,58 +4,109 @@ import time
 # from multiprocessing import Queue
 import psycopg2
 
+set_item_no = set()
 
-def connect_db():
+
+def connect_db(database):
     try:
-        conn = psycopg2.connect(database="db1", user="db1", password="db001", host="140.114.77.23", port="5432")
+        conn = psycopg2.connect(database=database, user="db1", password="db001", host="140.114.77.23", port="5432")
     except Exception:
         print("Unable to connect")
     return conn
 
 
-def prompt_user(cursor):
+def prompt_user(conn):
+    cursor = conn.cursor()
     while True:
-        print("1: Enter sql command directly")
-        print("2: Show all unclear events")
-        print("3: Enter correct location of event")
-        command = sys.stdin.readline()[:-1]
-        print()
+        print("\n1: Show all unclear events")
+        print("2: Enter correct location of event")
+        print("3: Enter sql command directly")
+        print("4: Quit")
+        command = input("Command: ")
+
         if command == "1":
-            print("Enter your sql command: ", end='')
-            sql = sys.stdin.readline()
-            cursor.execute(sql)
-            rows = cursor.fetchall()
-            print(rows)
+            cursor.execute("SELECT * FROM accident_event WHERE accident_status='not clear';")
+            for row in cursor.fetchall():
+                print(row)
+
         elif command == "2":
-            pass
+            event_id = input("Enter the event id: ")
+            try:
+                event_id = int(event_id)
+            except ValueError:
+                print("You must enter a integer")
+                continue
+            cursor.execute("SELECT count(*) FROM accident_event WHERE accident_id = %s", (event_id, ))
+            if cursor.fetchall()[0][0] == 0:
+                print("No such event id")
+                continue
+
+            longitude = input("Actual longitude: ")
+            latitude = input("Actual latitude: ")
+            try:
+                longitude = float(longitude)
+                latitude = float(latitude)
+            except ValueError:
+                print("You must enter a float")
+                continue
+
+            try:
+                sql = """UPDATE accident_event
+                SET actual_longitude = %s, actual_latitude= %s
+                WHERE accident_id = %s;"""
+                cursor.execute(sql, (longitude, latitude, event_id))
+            except psycopg2.Error as e:
+                print(e.diag.message_primary, end='\n\n')
+                continue
+
         elif command == "3":
-            pass
+            sql = input("\nEnter your sql command here: ")
+            try:
+                cursor.execute(sql)
+            except psycopg2.Error as e:
+                print(e.diag.message_primary, end='\n\n')
+                continue
+            for row in cursor.fetchall():
+                print(row)
+
         else:
             print("There's not command called " + command)
 
 
-def scan_view(cursor):
+def scan_view(conn):
+    global set_item_no
+    cursor = conn.cursor()
     while True:
         time.sleep(1)
-        print("scanning...")
-        # scan the view
+        sql = "SELECT * FROM accident_status_information;"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            pass
+            item_no = row[1]
+            if item_no not in set_item_no:
+                print("The accident", item_no, "is not in original set_item_no")
+                # insert into Accident Event
+                # calculate the closest HC/PO
+
+                set_item_no.add(item_no)
+                pass
 
 
-def foobar(cursor):
-    input_thread = threading.Thread(target=prompt_user, args=(cursor, ))
+def foobar():
+    conn1 = connect_db(database="db1")
+    input_thread = threading.Thread(target=prompt_user, args=(conn1, ))
     input_thread.daemon = True
     input_thread.start()
 
-    scan_thread = threading.Thread(target=scan_view, args=(cursor, ))
+    conn2 = connect_db(database="db2")
+    scan_thread = threading.Thread(target=scan_view, args=(conn2, ))
     scan_thread.daemon = True
     scan_thread.start()
 
     input_thread.join()
+    conn1.close()
+    conn2.close()
 
 
-conn = connect_db()
-cursor = conn.cursor()
-foobar(cursor)
-
-cursor.close()
-conn.close()
+foobar()
